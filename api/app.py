d69fa608292df9ad6a435f3a00370eb76053a339
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 19:14:45 -0300

    requieriments added to api folder

--- api/app.py
+++ api/app.py
@@ -10,6 +10,10 @@ def home():
 
 @app.route("/run-colab")
 def run_colab():
+    url = 'https://drive.google.com/u/0/uc?id=18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ&export=download'
+    file_name = 'tradingBot.ipynb'
+    response = requests.get(url)
+    open(file_name, 'wb').write(response.content)
     return jsonify(message='colab notebook ran successfully')
 
 
