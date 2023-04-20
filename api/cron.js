060152121f16a339b41ba01d84b9feda6836bcfb
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 16:10:57 -0300

    app.py changed

--- api/cron.js
+++ api/cron.js
@@ -4,9 +4,14 @@ export default async function handler(req, res) {
   try {
     const response = await fetch('https://trading-bot.vercel.app/run-colab');
     const data = await response.json();
-    res.status(200).send(data);
+    if (response.ok && data.message === 'colab notebook ran successfully') {
+      res.status(200).send(data);
+    } else {
+      res.status(500).send(data.message);
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send('Error running Colab notebook');
   }
 }
+
