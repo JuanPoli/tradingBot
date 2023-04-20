68840b716dad9bcf1edad9238044f14698eb9476
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 18:56:41 -0300

    requieriments added to api folder

--- api/app.py
+++ api/app.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify
 import gdown
+import requests
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "HELLO from vercel use flask"
+    return "HELLO from vercel use flask"
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://drive.google.com/file/d/18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ#scrollTo=osBHhVysOEzi', 'tradingBot.ipynb', quiet=False)
+    url = 'https://drive.google.com/u/0/uc?id=18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ&export=download'
+    response = requests.get(url)
+    with open('tradingBot.ipynb', 'wb') as f:
+        f.write(response.content)
     return jsonify(message='colab notebook ran successfully')
+
 
