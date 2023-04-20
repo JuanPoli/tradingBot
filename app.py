8d23bf376fbb4f1e44a86c37e942045bec699784
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 15:57:42 -0300

    app.py changed

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://drive.google.com/uc?id=18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ', 'tradingBot.ipynb', quiet=False)
-    return jsonify(message='colab notebook ran successfully')
+    return gdown.download('https://drive.google.com/uc?id=18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ', 'tradingBot.ipynb', quiet=False)
+    
