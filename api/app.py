dfad4f8f3027df2f9170a7ac1a96aa99763a5d28
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 20:37:38 -0300

    trying to download a file

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
-import nbconvert
-import os
+import requests
+
 
 app = Flask(__name__)
 
@@ -8,10 +8,9 @@ app = Flask(__name__)
 def home():
     return "HELLO from vercel use flask"
 
-@app.route('/run-colab')
+@app.route("/run-colab")
 def run_colab():
-    notebook_file = "tradingBot.ipynb"
-    python_file = "tradingBot.py"
-    nbconvert.export_python(notebook_file, python_file)
-    os.system(f"python {python_file}")
-
+    url = 'https://colab.research.google.com/drive/18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ?usp=sharing'
+    r = requests.get(url, allow_redirects=True)
+    open('tradingBot.ipynb', 'wb').write(r.content)
+    return jsonify(message='colab notebook ran successfully')
