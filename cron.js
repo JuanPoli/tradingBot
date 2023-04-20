ffd1b69eb7adfa5e8f8da91e409ac0e02c3fb2ef
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 15:18:55 -0300

    vercel file changed

--- cron.js
+++ cron.js
@@ -1,12 +1,3 @@
-import fetch from 'node-fetch';
-
-export default async function handler(req, res) {
-  try {
-    const response = await fetch('/app');
-    const data = await response.json();
-    res.status(200).send(data);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Error running Colab notebook');
-  }
+export default function handler(req, res) {
+  res.status(200).end('Hello Cron!');
 }
