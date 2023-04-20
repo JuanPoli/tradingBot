5fa9f3b43c61c5ca19698e6574e4dc13a6dd11a9
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 16:23:59 -0300

    cron.js file chnaged

--- api/cron.js
+++ api/cron.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://trading-bot.vercel.app/run-colab');
+    const response = await fetch('https://trading-bot-swart.vercel.app/run-colab');
     const data = await response.json();
     if (response.ok && data.message === 'colab notebook ran successfully') {
       res.status(200).send(data);
