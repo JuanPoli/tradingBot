d3f8608e6f007c07508e70ec684123bf7c862e5f
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 18:20:34 -0300

    vercel.json file changed

--- api/app.py
+++ api/app.py
@@ -1,23 +1,15 @@
-import requests
-from flask import Flask
-
+from flask import Flask, jsonify
+import gdown
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 	return "HELLO from vercel use flask"
 
+
+
 @app.route('/run-colab')
 def run_colab():
-    file_id = '18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ#scrollTo=osBHhVysOEzi'
-    url = f'https://colab.research.google.com/drive/18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ#scrollTo=osBHhVysOEzi'
-    response = requests.get(url)
-    content = response.content
+    gdown.download('https://drive.google.com/file/d/18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ#scrollTo=osBHhVysOEzi', 'tradingBot.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')
 
-    with open('tradingBot.ipynb', 'wb') as f:
-        f.write(content)
-
-    return 'Colab file downloaded successfully!'
-
-if __name__ == '__main__':
-    app.run()
