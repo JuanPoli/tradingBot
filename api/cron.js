85caf64adc2fef58dfe98e43076c856029c0ff7f
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 15:30:08 -0300

    cron.js file changed

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).end('Hello Cron!');
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://trading-bot.vercel.app/run-colab');
+    const data = await response.json();
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error running Colab notebook');
+  }
 }
