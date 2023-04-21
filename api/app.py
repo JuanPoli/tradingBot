d9d29d18d4ba21f4f7ba2f4d704ec3fe59c6cce0
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 21:41:20 -0300

    trying to download a file

--- api/app.py
+++ api/app.py
@@ -9,6 +9,6 @@ def home():
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://drive.google.com/file/d/18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ', 'colab.ipynb', quiet=False)
+    gdown.download('https://drive.google.com/uc?id=18jK7gSM1Lv-AVYEoHmeDesaRO0eJhZkJ', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
 
