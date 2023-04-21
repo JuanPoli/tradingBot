f9e228c8fa7360df03a96581112032bc3b76ab11
Juan Cruz Poli <juancruzpoli@gmail.com>
2023-04-20 21:44:42 -0300

    trying to download a file

--- api/cron.js
+++ api/cron.js
@@ -1,16 +1,13 @@
 const fetch = require('node-fetch');
 
-console.log("Starting cron job...");
-
 fetch('https://trading-bot-swart.vercel.app/run-colab')
   .then(response => {
-    console.log("Response status: " + response.status);
-    return response.json();
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Response not OK');
+    }
   })
-  .then(data => {
-    console.log("Response data: " + JSON.stringify(data));
-  })
-  .catch(error => {
-    console.error("Error: " + error);
-  });
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 
